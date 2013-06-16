523889e750bc2ed99419ed5a117c04bd6e496e90
Shawn Deprey <shawndeprey@gmail.com>
2013-06-15 19:00:53 -0700

    Set up our testing environment and finished up user accounts.

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,12 @@
 
 FactoryGirl.define do
   factory :user do
+  	sequence :email do |n|
+      "user_#{n}@test.knuvu.com"
+    end
+    password "password"
+    password_confirmation "password"
+    remember_me true
+    admin false
   end
 end
