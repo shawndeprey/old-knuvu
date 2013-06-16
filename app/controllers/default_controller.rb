872d29a19463f791b2d6227124753cc2def8321e
Shawn Deprey <shawndeprey@gmail.com>
2013-06-15 23:16:34 -0700

    Added base web app template and content. Also styled the user forms.

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,6 +1,31 @@
 class DefaultController < ApplicationController
 
   def index
+  	# index.html.erb
+  end
+
+  def about
+  	# about.html.erb
+  end
+
+  def professional_services
+  	# professional_services.html.erb
+  end
+
+  def products
+  	# products.html.erb
+  end
+
+  def contact_us
+  	# contact_us.html.erb
+  end
+
+  def consulting_and_transactional_services
+  	# consulting_and_transactional_services.html.erb
+  end
+
+  def board_of_directors
+  	# board_of_directors.html.erb
   end
 
 end
--- test/functional/controllers/default_controller_test.rb
+++ test/functional/controllers/default_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class DefaultControllerTest < ActionController::TestCase
+	include Devise::TestHelpers
   setup do
     # @user = FactoryGirl.create(:user, admin: true)
   end
@@ -8,6 +9,43 @@ class DefaultControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_match(/KnuVu is a Quantitative Qualitative Multivariate Heatmapping Decision Tool/, response.body)
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_match(/We started KnuVu because we found it to be a super useful/, response.body)
+  end
+
+  test "should get professional_services" do
+    get :professional_services
+    assert_response :success
+    assert_match(/KnuVu currently has a bench of approximately 40 consultants ranging/, response.body)
+  end
+
+  test "should get products" do
+    get :products
+    assert_response :success
+    assert_match(/KnuVu creates a New View into decision making/, response.body)
+  end
+
+  test "should get contact_us" do
+    get :contact_us
+    assert_response :success
+    assert_match(/<strong>Contact Us<\/strong>/, response.body)
+  end
+
+  test "should get consulting_and_transactional_services" do
+    get :consulting_and_transactional_services
+    assert_response :success
+    assert_match(/KnuVu is an evolution of brauning/, response.body)
+  end
+
+  test "should get board_of_directors" do
+    get :board_of_directors
+    assert_response :success
+    assert_match(/Mike Silva is a Mobile \& Emerging Technologies Strategy Consultant/, response.body)
   end
 
 end
